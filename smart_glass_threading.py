a45c89acc6ecb0c058eed059a875fadf1cd3d081
Joyce Zheng <jy6zheng@uwaterloo.ca>
2023-03-01 21:11:00 -0500

    added timer mode for threading

--- smart_glass_threading.py
+++ smart_glass_threading.py
@@ -4,6 +4,7 @@ from threading import Thread
 import time
 from transparency import *
 from light_sensor import *
+from time import *
 
 # right now can either be 1 (fully transparent) or 0 (completely opaque)
 CURRENT_STATE = 0
@@ -20,7 +21,7 @@ transparency.start()
 
 def light_sensor_mode(difference):
         # NOTE: going to add feature that considers magnitude
-        # positive indicates outside is righter
+        # positive indicates outside is brighter
         if difference > 1:
             print("increase natural light")
             return 1
@@ -47,6 +48,26 @@ while True:
                 CURRENT_STATE = light_sensor_mode(difference)
         except KeyboardInterrupt:
             continue
+    elif console_input == "3":
+        print("timer mode")
+        setting = input("transparent (1) or opaque (0): ")
+        setting_time = input("input time as s:m:h ")
+        setting_time = setting_time.split(":")
+        time_sec = 0
+        for i in range(len(setting_time)):
+            time_sec += int(setting_time[i]) * 60 ** i
+        print("changing transparency for {} seconds".format(time_sec))
+        CURRENT_STATE = int(setting)
+        sleep(time_sec)
+        print("timer finished")
+        if CURRENT_STATE > 0:
+            CURRENT_STATE = 0
+            print("turning opaque")
+        else:
+            CURRENT_STATE = 1
+            print("turning transparent")
+        
+        
         
                 
                 
